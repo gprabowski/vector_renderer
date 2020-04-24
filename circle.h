aa33213d6512a44334061de52836bd239fe1417b
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-04-24 20:35:36 +0200

    line drawing/editing with midpoint algorithm, circle drawing/editing with midpoint algorithm, reset functionality

---
+++ circle.h
@@ -0,0 +1,11 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+
+class circle : public drawable
+{
+public:
+    circle();
+};
+
+#endif // CIRCLE_H
---
+++ utility_func.h
@@ -0,0 +1,4 @@
+#ifndef UTILITY_FUNC_H
+#define UTILITY_FUNC_H
+
+#endif // UTILITY_FUNC_H
